3c694b3b5134bb557d98b11c3be19aa04201c3a8
wubmans <wubmans@outlook.com>
2024-02-07 14:58:48 +0100

    poekie

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 let grid = []
 let sizeX
 let sizeY
-let maxAge = 255
+let maxAge = 256
 
 let direction = false
 let string = "VAN KLEEF-DIEBEN"
@@ -30,7 +30,7 @@ function setup() {
 		grid[i] = []
 		for (let j = 0; j < sizeY; j++) 
 		{
-			grid[i][j] = { letter: null, age: 0 }
+			grid[i][j] = { letter: null, age: 0, class: null }
 		}
 	}
 
@@ -65,12 +65,12 @@ function vkd(x, y)
 
 		if (direction)
 		{
-			grid[x + i][y] = { letter: s[i], age: 300, color: color("#444"), count: 4, class: "source" }
+			grid[x + i][y] = { letter: s[i], age: maxAge, color: color("#444"), count: 4, class: "source" }
 		
 		}
 		else
 		{
-			grid[x][y + i] = { letter: s[i], age: 300, color: color("#444"), count: 4, class: "source" }
+			grid[x][y + i] = { letter: s[i], age: maxAge, color: color("#444"), count: 4, class: "source" }
 		}
 
 		
@@ -105,18 +105,30 @@ function random_vkd()
 function getNeighbors(x, y, r)
 {
 	let neighbors = []
-	for (let i = x - r + 1; i < x + r; i++) 
+	for (let i = - r ; i < r + 1; i++) 
 	{
-		for (let j = y - r + 1; j < y + r; j++) 
+		for (let j = - r; j < r + 1; j++) 
 		{
-			if (i < 0 || i >= sizeX || j < 0 || j >= sizeY)
+			if (x + i < 0 || x + i >= sizeX || y + j < 0 || y + j >= sizeY)
 			{
 				continue;
 			}
 
-			if (grid[i][j].letter !== null)
+			if ((i * i) + (j * j) > r * r)
 			{
-				neighbors.push(grid[i][j])
+				continue
+			}
+
+			let point = grid[x + i][y + j]
+
+			if (point.letter !== null)
+			{
+				if (point.class === "source" && point.age > maxAge * 0.9)
+				{
+					continue;
+				}
+				
+				neighbors.push(point)
 			}
 		} 
 	}
@@ -138,64 +150,95 @@ function calculateFood(x, y)
 	return food
 }
 
+let frameCount = 0;
+
 function draw() {
+
+	frameCount++
 	
 	clear()
+
+				
 	for (let i = 0; i < sizeX; i++) 
 	{
 		for (let j = 0; j < sizeY; j++) 
 		{
-			if (grid[i][j].letter !== null)
-			{
-				let c = grid[i][j].color
-				c.setAlpha(grid[i][j].age)
-				fill(c)
-				text(grid[i][j].letter, i * dX, j * dY)
 
-				if (grid[i][j].class === "food")
+			let point = grid[i][j]
+
+			// if (frameCount % 30 === 0)
+			// {
+			point.age--
+			// }
+			
+
+			if (point.age < 0)
+			{
+				point.letter = null
+				point.class = null
+			}
+
+			if (point.class === "source")
+			{
+				// if (frameCount % 30 === 0)
+				// {
+				point.age -= random() * 4
+				// }
+
+				if (point.age < maxAge * 0.4)
 				{
-					let chars = [ "░",  "▒", "▓",  "█", ".", ]
-					grid[i][j].letter = chars[(grid[i][j].age / maxAge * chars.length | 0)]
+					point.age -= 3
 				}
-				
-				
-				if (grid[i][j].class === "source" && grid[i][j].age < 200)
+
+				if (point.age > maxAge * 0.9 || point.age < maxAge * 0.4)
 				{
 					chars =[ ":", "."]
-					grid[i][j].age -= random() * 3
+					
 					if (random() * 20 > 15) 
 					{
-						grid[i][j].letter = chars[random() * chars.length | 0]
-					}
-					
+						point.output = chars[random() * chars.length | 0]
+					}					
 				}
-
-				//if (random() * 200 > 150)
-				//{
-					grid[i][j].age--
-				//}
-
-				if (grid[i][j].age < 0)
-				{
-					grid[i][j].letter = null
+				else {
+					point.output = point.letter
 				}
-
 			}
-			
-			else 
+
+			if (point.class === "food")
 			{
+				let chars = [ "░",  "▒", "▓",  "█" ]
+				let index = min(1.3 * point.age / maxAge * chars.length | 0, chars.length - 1)
+				point.output = chars[index]
+			}
+
+			let c = point.color || color(255)
+			c.setAlpha(point.age)
+			fill(c)
+			text(point.output || "", i * dX, j * dY)
+			
+			if (point.class !== "source") 
+			{				
+
 				
 				let food = calculateFood(i, j);
-				if (food > 130) {
-					grid[i][j] = { 
-						letter: "X",
-						age: food * 0.9, 
-						color: color(128, 128, 128),
-						class: "food"
-					}
+				// console.log(food)
+
+				if (random(1000) < 999 - food / 30) 
+				{
+					//continue;
 				}
+
+				if (food !== 0 ) {
+					point.letter = "█"	
+					point.class = "food"
+					point.color = color(128, 128, 128)
+					point.age = min(maxAge - 1, max(point.age, food * 0.8 * random(0.8, 1.0)))
 				
-			}
+				}
+			//}
+
+		}
+			
 		}
 		
 	}
@@ -204,4 +247,8 @@ function draw() {
 	{
 		random_vkd()
 	}
+
+
+	fill("red")
+	text(frameCount, 100, 0)
 }
