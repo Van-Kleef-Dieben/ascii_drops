665d0f355059fb3fe15b9a2e10619e3fc217c64f
= <=>
2024-02-08 13:16:34 +0100

    drops spreading 1

--- sketch.js
+++ sketch.js
@@ -15,6 +15,8 @@ let font = "iA Writer Mono"
 const dX = 15
 const dY = 23
 
+const fp = 0.01
+
 function setup() {
 
 	let w = displayWidth
@@ -36,240 +38,103 @@ function setup() {
 		{
 			grid[i][j] = 
 			{ 
-				letter: null, 
-				age: 0, 
-				class: null, 
-				q_age : 0 
+				amount: 0,
+				_amount: 0
 			}
 		}
 	}
 
-	vkd((sizeX / 2 - string.length / 2)| 0, sizeY / 2 | 0 )
+	// vkd((sizeX / 2 - string.length / 2)| 0, sizeY / 2 | 0 )
 }
 
-function vkd(x, y)
+function mouseClicked()
 {
-	let s = string
-	let v 
-	let max
-	
-	direction = !direction
+	let x = mouseX / dX | 0
+	let y = mouseY / dY | 0
 
-	if (direction)
+	if (x >= sizeX || y >= sizeY)
 	{
-		v = x
-		max = sizeX
-	}
-	else
-	{
-		v = y
-		max = sizeY
+		return;
 	}
 
-	for (let i = 0; i < s.length; i++) {
-
-		if (v + i >= max)
-		{
-			continue
-		}
-
-		let point = { 
-			letter: s[i], 
-			age: maxAge * 1.2, 
-			color: color("black"), 
-			class: "source",
-			q_age: 0
-		}
-
-		if (direction)
-		{
-			grid[x + i][y] = point
-		
-		}
-		else
-		{
-			grid[x][y + i] = point
-		}
-		
-	}
+	grid[x][y].amount = 4300
 }
 
-function random_vkd() 
+function updateGrid()
 {
-	lastVKDframeCount = frameCount
-
-	let x
-	let y
-
-	let found = false
-
-	while(!found)
+			
+	for (let i = 0; i < sizeX; i++) 
 	{
-		x = random(sizeX) | 0
-		y = random(sizeY - 4) | 0  + 2
-
-		if (y > (sizeY - string.length / 2))
+		for (let j = 0; j < sizeY; j++) 
 		{
-			continue
-		}
 
-		// found = getNeighbors(x, y, 5).length > 2
-		found = true
+			let p0 = grid[i][j]
+			p0._amount += p0.amount
+			let flow = 0
+
+			for (const {a, b } of [{a: -1, b: 0}, {a: 1, b: 0}, {a: 0, b: -1}, {a: 0, b: 1}])
+			{
+				if (i + a >= 0 && i + a < sizeX && j + b >= 0 && j + b < sizeY)
+				{
+					let p1 = grid[i + a][j + b];
+
+					if (p1.amount < p0.amount)
+					{
+						let f = (p0.amount - p1.amount) * fp
+						p1._amount += f
+						flow += f
+						p0._amount -= f
+					}
+				}
+				
+			}
+		
+			
+
+		}
 	}
 
-	vkd(x, y)
-
+	for (let i = 0; i < sizeX; i++) 
+	{
+		for (let j = 0; j < sizeY; j++) 
+		{
+			let p = grid[i][j]
+			p.amount = p._amount
+			p._amount = 0
+		}
+	}
 }
 
-function draw() {
+function drawGrid()
+{
+	for (let i = 0; i < sizeX; i++) 
+	{
+		for (let j = 0; j < sizeY; j++) 
+		{
+			let p = grid[i][j]
+
+			const chars = [ "", ".", "░",  "▒", "▓",  "█" ]
+			const index = min((p.amount / maxAge * chars.length) | 0, chars.length - 1)
+			
+			textAlign("left, top")
+			const c = color("black")
+			fill(c)
+			text(chars[index], i * dX, j * dY)
+		}
+	}
+}
+
+function draw() 
+{
 
 	frameCount++
-	
+
 	clear()
 	background(255)
+	
+	updateGrid()
+	drawGrid()
 
-	if (lastVKDframeCount + vkdPeriod < frameCount)
-	{
-		random_vkd()
-	}
-				
-	for (let i = 0; i < sizeX; i++) 
-	{
-		for (let j = 0; j < sizeY; j++) 
-		{
-
-			let point = grid[i][j]
-
-			if (point.age < 15)
-			{
-				point.age = 0
-				point.q_age = 0
-			}
-
-			if (point.class === "source")
-			{
-				// if (frameCount % 30 === 0)
-				// {
-				point.age *= 0.9
-
-				if (random(100) > 90)
-				{
-					point.age *= 0.9
-				}
-				// }
-
-				if (point.age < maxAge * 0.5){
-					point.age *= 0.9
-				}
-				
-
-				if (point.age >= maxAge || point.age < maxAge * 0.5)
-				{
-					chars =[ ":", ".", ""]
-					
-					if (random() * 20 > 15) 
-					{
-						point.output = chars[random() * chars.length | 0]
-					}					
-				}
-				else 
-				{
-					point.output = point.letter
-				}
-
-				
-			}
-
-			else if (point.age > 0)
-			{
-				//if (frameCount % 5 === 0)
-				// {
-				point.age *= 0.95
-				// }
-			
-				let chars = [ "░",  "▒", "▓",  "█" ]
-				let index = min(point.age / maxAge * chars.length | 0, chars.length - 1)
-				point.output = chars[index]
-
-				
-			}
-			let c = color(0)
-			// c.setAlpha(min(point.age, 255))
-			fill(c)
-			
-			text(point.output || "", i * dX, j * dY)
-
-			
-			
-			// if (point.class !== "source") 
-			// {				
-				
-			// 	//let food = calculateFood(i, j);
-			// 	// console.log(food)
-
-			// 	// if (random(1000) < 999 - food / 30) 
-			// 	// {
-			// 	// 	//continue;
-			// 	// }
-
-			// 	if (food > 0 ) {
-			// 		point.letter = "█"	
-			// 		point.class = "food"
-			// 		point.color = color(150, 100, 100)
-
-			// 		{
-			// 			point.age = min(maxAge, (point.age * 2 + food)/  3 )
-			// 		}
-
-					
-				
-			// 	}
-			//}
-
-		
-			
-		}
-		
-	}
-
-	for (let i = 0; i < sizeX; i++) 
-	{
-		for (let j = 0; j < sizeY; j++) 
-		{
-			let point = grid[i][j]
-			// if (point.age !== 0)
-			{
-				for (let a of [-1, 1 ])
-				{
-					if (i + a >= 0 && i + a < sizeX)
-					{
-						grid[i + a][j].q_age += 0.15 * point.age
-					}
-										
-					if (j + a >= 0 && j + a < sizeY)
-					{
-						grid[i][j + a].q_age += 0.15 * point.age
-					}
-					
-					point.age *= 0.8
-				}
-			}
-			
-		}
-	}
-
-	for (let i = 0; i < sizeX; i++) 
-	{
-		for (let j = 0; j < sizeY; j++) 
-		{
-			let point = grid[i][j]
-			point.age += point.q_age 
-			point.q_age = 0
-		}
-	}
-
-
-
-	// fill("red")
-	// text(frameCount, 100, 0)
 }
+	
+
